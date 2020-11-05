00971199f2c84d525102b9f4b969cfbd6531441e
htucker98 <hanavtucker@gmail.com>
2020-11-05 12:06:08 -0500

    Complete A1 with test script

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+java Main -f test1.ps
+
+java Main -f test2.ps -a 0 -b 0 -c 499 -d 499 -s 0.8 -m 85 -n 25 -r 10
+
+java Main -f test3.ps -s 0.5
+
+java Main -f test4.ps -r -30
+
+java Main -f test5.ps -m 100 -n 100
+
